3c27dcd63a863be6aecf5d2949471aa23311820f
Vijay Krishan Pandey <v.k.pandeycool98@gmail.com>
2020-10-01 00:21:59 +0530

    Update CountingElements.cpp

--- CountingElements.cpp
+++ CountingElements.cpp
@@ -6,8 +6,8 @@ public:
         
         map<int,int>mp;
         
-        for(int i=0;i<arr.size();i++){
-            mp[arr[i]]++;
+        for(auto it:arr){
+            mp[it]++;
         }
         
         int ans=0;
